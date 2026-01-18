936c197c1a053d1fe6d5ed68fcc995466fa6c5c3
j4ckgrey <j4ckgrey@gmail.com>
2026-01-18 01:36:43 +0100

    Release v0.1.1.8: Hotfix for non-x64 systems

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.1.7")]
-[assembly: AssemblyFileVersion("0.1.1.7")]
+[assembly: AssemblyVersion("0.1.1.8")]
+[assembly: AssemblyFileVersion("0.1.1.8")]
 [assembly: AssemblyProduct("Cavea")]
 [assembly: AssemblyCompany("j4ckgrey")]
